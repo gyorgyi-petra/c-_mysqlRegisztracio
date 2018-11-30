1678b28c99d43aba46aa5143732709de8347f7fc
gyorgyi-petra <44090425+gyorgyi-petra@users.noreply.github.com>
2018-11-30 16:30:17 +0100

    Add files via upload
    2018-11-30 óraimunka

--- MysqlApp/MysqlApp/Form1.Designer.cs
+++ MysqlApp/MysqlApp/Form1.Designer.cs
@@ -35,16 +35,23 @@
             this.delButton = new System.Windows.Forms.Button();
             this.updateButton = new System.Windows.Forms.Button();
             this.del_form = new System.Windows.Forms.TextBox();
-            this.update_form = new System.Windows.Forms.TextBox();
+            this.nevupdate_form = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.jelszoupdate_form = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reg_userek_label = new System.Windows.Forms.Label();
+            this.regisztraltak_list = new System.Windows.Forms.ListBox();
+            this.stat_list = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // nev_form
             // 
             this.nev_form.Location = new System.Drawing.Point(27, 38);
             this.nev_form.Name = "nev_form";
-            this.nev_form.Size = new System.Drawing.Size(245, 20);
+            this.nev_form.Size = new System.Drawing.Size(434, 20);
             this.nev_form.TabIndex = 0;
             // 
             // jelszo_form
@@ -52,19 +59,19 @@
             this.jelszo_form.Location = new System.Drawing.Point(27, 64);
             this.jelszo_form.Name = "jelszo_form";
             this.jelszo_form.PasswordChar = '*';
-            this.jelszo_form.Size = new System.Drawing.Size(245, 20);
+            this.jelszo_form.Size = new System.Drawing.Size(434, 20);
             this.jelszo_form.TabIndex = 1;
             // 
             // szuletesiido
             // 
             this.szuletesiido.Location = new System.Drawing.Point(27, 91);
             this.szuletesiido.Name = "szuletesiido";
-            this.szuletesiido.Size = new System.Drawing.Size(245, 20);
+            this.szuletesiido.Size = new System.Drawing.Size(434, 20);
             this.szuletesiido.TabIndex = 2;
             // 
             // regButton
             // 
-            this.regButton.Location = new System.Drawing.Point(197, 130);
+            this.regButton.Location = new System.Drawing.Point(357, 140);
             this.regButton.Name = "regButton";
             this.regButton.Size = new System.Drawing.Size(75, 23);
             this.regButton.TabIndex = 3;
@@ -74,7 +81,7 @@
             // 
             // delButton
             // 
-            this.delButton.Location = new System.Drawing.Point(197, 181);
+            this.delButton.Location = new System.Drawing.Point(357, 318);
             this.delButton.Name = "delButton";
             this.delButton.Size = new System.Drawing.Size(75, 23);
             this.delButton.TabIndex = 4;
@@ -83,7 +90,7 @@
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(197, 226);
+            this.updateButton.Location = new System.Drawing.Point(357, 406);
             this.updateButton.Name = "updateButton";
             this.updateButton.Size = new System.Drawing.Size(75, 23);
             this.updateButton.TabIndex = 5;
@@ -92,22 +99,22 @@
             // 
             // del_form
             // 
-            this.del_form.Location = new System.Drawing.Point(27, 181);
+            this.del_form.Location = new System.Drawing.Point(27, 320);
             this.del_form.Name = "del_form";
-            this.del_form.Size = new System.Drawing.Size(100, 20);
+            this.del_form.Size = new System.Drawing.Size(180, 20);
             this.del_form.TabIndex = 6;
             // 
-            // update_form
+            // nevupdate_form
             // 
-            this.update_form.Location = new System.Drawing.Point(27, 229);
-            this.update_form.Name = "update_form";
-            this.update_form.Size = new System.Drawing.Size(100, 20);
-            this.update_form.TabIndex = 7;
+            this.nevupdate_form.Location = new System.Drawing.Point(30, 383);
+            this.nevupdate_form.Name = "nevupdate_form";
+            this.nevupdate_form.Size = new System.Drawing.Size(180, 20);
+            this.nevupdate_form.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 162);
+            this.label1.Location = new System.Drawing.Point(30, 291);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 8;
@@ -116,20 +123,75 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 210);
+            this.label2.Location = new System.Drawing.Point(30, 355);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(79, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Módisítás:";
+            this.label2.Text = "Név módosítás";
+            // 
+            // jelszoupdate_form
+            // 
+            this.jelszoupdate_form.Location = new System.Drawing.Point(30, 441);
+            this.jelszoupdate_form.Name = "jelszoupdate_form";
+            this.jelszoupdate_form.Size = new System.Drawing.Size(180, 20);
+            this.jelszoupdate_form.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Jelszó módosítás";
+            // 
+            // reg_userek_label
+            // 
+            this.reg_userek_label.AutoSize = true;
+            this.reg_userek_label.Location = new System.Drawing.Point(27, 140);
+            this.reg_userek_label.Name = "reg_userek_label";
+            this.reg_userek_label.Size = new System.Drawing.Size(165, 13);
+            this.reg_userek_label.TabIndex = 12;
+            this.reg_userek_label.Text = "PLACEHOLDER_REG_USEREK";
+            // 
+            // regisztraltak_list
+            // 
+            this.regisztraltak_list.FormattingEnabled = true;
+            this.regisztraltak_list.Location = new System.Drawing.Point(27, 168);
+            this.regisztraltak_list.Name = "regisztraltak_list";
+            this.regisztraltak_list.Size = new System.Drawing.Size(165, 121);
+            this.regisztraltak_list.TabIndex = 13;
+            // 
+            // stat_list
+            // 
+            this.stat_list.FormattingEnabled = true;
+            this.stat_list.Location = new System.Drawing.Point(209, 168);
+            this.stat_list.Name = "stat_list";
+            this.stat_list.Size = new System.Drawing.Size(189, 121);
+            this.stat_list.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(433, 168);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(719, 532);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.stat_list);
+            this.Controls.Add(this.regisztraltak_list);
+            this.Controls.Add(this.reg_userek_label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.jelszoupdate_form);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.update_form);
+            this.Controls.Add(this.nevupdate_form);
             this.Controls.Add(this.del_form);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.delButton);
@@ -139,6 +201,7 @@
             this.Controls.Add(this.nev_form);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,9 +216,15 @@
         private System.Windows.Forms.Button delButton;
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.TextBox del_form;
-        private System.Windows.Forms.TextBox update_form;
+        private System.Windows.Forms.TextBox nevupdate_form;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox jelszoupdate_form;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label reg_userek_label;
+        private System.Windows.Forms.ListBox regisztraltak_list;
+        private System.Windows.Forms.ListBox stat_list;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- MysqlApp/MysqlApp/Form1.cs
+++ MysqlApp/MysqlApp/Form1.cs
@@ -13,9 +13,96 @@ namespace MysqlApp
 {
     public partial class Form1 : Form
     {
+
+
+        const string AdatLetrehozasSql = @"
+            CREATE TABLE IF NOT EXISTS felhasznalo (
+            
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            nev VARCHAR(80) NOT NULL UNIQUE,
+            jelszo VARCHAR(100) NOT NULL,
+            regdatum DATE NOT NULL 
+    )
+";
+
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                conn = new MySqlConnection("Server=localhost;Database=regisztracio;UID=root;Pwd=");
+
+                conn.Open();
+
+                var letrehozas = conn.CreateCommand();
+                letrehozas.CommandText = AdatLetrehozasSql;
+                letrehozas.ExecuteNonQuery();
+            }
+            catch (MySqlException ex) {
+                MessageBox.Show("Adatbázis hiba: \n" + ex.Message);
+                this.Close();
+            }
+        }
+
+        public void UpdateRegUserek() {
+            string kiirando = "Regisztralt {0} db felhastnalo: ";
+            var c = conn.CreateCommand();
+            c.CommandText = " SELECT COUNT(*) FROM felhasznalo ";
+            long darabszam = (long)c.ExecuteScalar();
+
+            reg_userek_label.Text = string.Format(kiirando, darabszam);
+
+            var cFelhasznalok = conn.CreateCommand();
+            cFelhasznalok.CommandText = "SELECT nev,regdatum FROM felhasznalo ORDER BY nev";
+            using (var reader = cFelhasznalok.ExecuteReader())
+            {
+
+                regisztraltak_list.Items.Clear();
+                while (reader.Read())
+                {
+                    var nev = reader.GetString("nev");
+                    var datum = reader.GetDateTime("regdatum");
+                    regisztraltak_list.Items.Add(
+                            string.Format(" {1:yyyy.MM.dd.} - {0}", nev, datum)
+                        );
+                }
+            }
+
+            var cStat = conn.CreateCommand();
+            cStat.CommandText = "SELECT regdatum, COUNT(*) db FROM felhasznalo GROUP BY regdatum ORDER BY regdatum";
+            using (var reader  = cStat.ExecuteReader()) {
+                stat_list.Items.Clear();
+                while (reader.Read()) {
+                    DateTime datum = reader.GetDateTime("regdatum");
+                    long db = reader.GetInt64("db");
+                    stat_list.Items.Add(
+                        string.Format(" {0:yyyy.MM.dd.} - {1}" , datum,db)
+                        );
+                };
+            }
+
+            var adapter = new MySqlDataAdapter("SELECT nev,regdatum FROM felhasznalo ORDER BY nev", conn);
+            var builder = new MySqlCommandBuilder(adapter);
+            var table = new DataTable();
+          
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+
+            table.RowChanged += (sender, args) =>
+            {
+                long id = (long)args.Row["id"];
+                string nev = (string)args.Row["nev"];
+                DateTime datum = (DateTime)args.Row["regdatum"];
+                var cUpdate = conn.CreateCommand();
+                cUpdate.CommandText = "UPDATE felhasznalo SET nev = @nev, jelszo = @jelszo , regdatum = @regdatum WHERE id = @id";
+                cUpdate.Parameters.AddWithValue("@nev", nev);
+                cUpdate.Parameters.AddWithValue("@regdatum", datum);
+                cUpdate.Parameters.AddWithValue("@id", id);
+                cUpdate.ExecuteNonQuery();
+            };
 
         }
 
@@ -25,12 +112,14 @@ namespace MysqlApp
             string jelszo = jelszo_form.Text;
             DateTime regdatum = szuletesiido.Value;
 
-            using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;UID=root;Pwd="))
-            {
-                conn.Open();
+           
+               
+
+                
+
 
                 var ellenorzes = conn.CreateCommand();
-                ellenorzes.CommandText = "select count(*) from felhasznalo where nev = @nev";
+                ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE nev = @nev";
                 ellenorzes.Parameters.AddWithValue("@nev", nev);
                 var darab = (long)ellenorzes.ExecuteScalar();
 
@@ -43,14 +132,13 @@ namespace MysqlApp
 
 
                 var command = conn.CreateCommand();
-                command.CommandText = "insert into felhasznalo (nev, jelszo, regdatum) values (@nev,@jelszo,@regdatum)";
+                command.CommandText = "INSERT INTO felhasznalo (nev, jelszo, regdatum) VALUES (@nev,@jelszo,@regdatum)";
                 command.Parameters.AddWithValue("@nev", nev);
                 command.Parameters.AddWithValue("@jelszo", jelszo);
                 command.Parameters.AddWithValue("@regdatum", regdatum);
                 int erintettsorok = command.ExecuteNonQuery();
 
-
-            }
+            UpdateRegUserek();
 
         }
             private void delButton_Click(object sender, EventArgs e)
@@ -63,9 +151,11 @@ namespace MysqlApp
                 conn.Open();
 
                 var torles_command = conn.CreateCommand();
-                torles_command.CommandText = "delete from felhasznalo where nev = @nev";
+                torles_command.CommandText = "DELETE FROM felhasznalo WHERE nev = @nev";
                 torles_command.Parameters.AddWithValue("@nev", nev);
                 int erintettsorok = torles_command.ExecuteNonQuery();
+
+                UpdateRegUserek();
             }
         }
 
@@ -79,9 +169,11 @@ namespace MysqlApp
                 conn.Open();
 
                 var command = conn.CreateCommand();
-                command.CommandText = "update felhasznalo set nev = @nev, jelszo = @jelszo , regdatum = @regdatum where nev = @nev";
+                command.CommandText = "UPDATE felhasznalo SET nev = @nev, jelszo = @jelszo , regdatum = @regdatum WHERE nev = @nev";
                 command.Parameters.AddWithValue("@nev", nev);
                 int erintettsorok = command.ExecuteNonQuery();
+
+                UpdateRegUserek();
             }
 
         }
